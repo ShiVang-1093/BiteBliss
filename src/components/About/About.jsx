1368198a5cce3f003f26131693f573b80923175a
Shivang <shivanggopalpatel@gmail.com>
2023-10-07 11:32:05 +0530

    Added testimonials and done ui changes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ContactForm from './contactForm';
+import Testimonial from './Testimonial';
+import Reviews from './Reviews';
 
 const About = () => {
   return (
     <>
+    <Testimonial/>
+    <Reviews/>
     <ContactForm />
+    
     </>
   )
 }
