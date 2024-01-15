06f7cad53c7de9235527918451f9bae922acf6d1
Shivang Patel <shivanggopalpatel@gmail.com>
2024-01-15 23:55:23 +0530

    .

---
+++ backend/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const dbconnection = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Database connected');
+  } catch (err) {
+    console.error('Connection failed:', err);
+    process.exit(1);
+  }
+};
+
+module.exports = dbconnection;
